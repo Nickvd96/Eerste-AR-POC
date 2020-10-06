526fd8770d37971101f94f3ee41d37c84e0b4f6e
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2020-10-06 12:21:32 +0200

    eerste script aangemaakt
    script met 2 arrays voor  AR balloons

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
+    public Transform[] spawnPpints;
+    public GameObject[] balloons;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
