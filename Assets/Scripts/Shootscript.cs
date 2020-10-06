e6003b7dae079a32a712ba09e2bd51360783bbfd
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2020-10-06 14:17:04 +0200

    srcript gemaakt voor het vliegen van ballonnen en het schieten van de balonnen

---
+++ Assets/Scripts/Shootscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootscript : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject smoke;
+    // Start is called before the first frame update
+    public void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            Destroy(hit.transform.gameObject);
+
+            Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+    }
+}
