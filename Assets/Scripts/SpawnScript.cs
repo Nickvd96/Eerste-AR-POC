711e180a6150a05a3f58378ce8288a4ceb3f6b24
i356100@fhict.nl <n.vandriel@student.fontys.nl>
2020-10-06 13:20:04 +0200

    aanpassingen gedaan aan editor
    heb extensies en instellingen meoten veranderen zodat monobehavior werkte in visual studio code

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    public Transform[] spawnPpints;
+    public Transform[] spawnPoints;
     public GameObject[] balloons;
 
     // Start is called before the first frame update
@@ -12,5 +12,14 @@ public class SpawnScript : MonoBehaviour
     {
         
     }
+     IEnumerator StartSpawning()
+    {
+        yield return new WaitForSecond(4);
 
+        for(int i = 0; i < 3; i++)
+        {
+            Instantiate(balloons[1], spawnPoints[i].position, Quaternion.identity);
+        }
+        StartCoroutine(StartSpawning());
+    }
 }
